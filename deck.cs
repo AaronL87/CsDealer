44dc539eebeb705058fa5d561ce297b04b911d73
Aaron Liftig <49658070+AaronL87@users.noreply.github.com>
2020-09-07 22:20:22 -0400

    Create deck.cs

---
+++ deck.cs
@@ -0,0 +1,200 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Deck : Stack
+{
+    //===============================================================================
+    // Deck Class
+    //===============================================================================
+ 
+    public new List<Card> cards;
+    public bool jokers;
+    public int numJokers;
+    public bool build;
+    public bool rebuild;
+    public bool reshuffle;
+    public new Dictionary<string, Dictionary<string, int>> ranks;
+    public int decksUsed;
+    
+    public Deck(object cards = null, bool jokers = false, int numJokers = 0, 
+        bool build = true, bool rebuild = false, bool reshuffle = false,
+        Dictionary<string, Dictionary<string, int>> ranks = null)
+    {
+        if (cards == null)
+        {
+            cards = new List<Card>();
+        }
+        
+        if (ranks == null)
+        {
+            ranks = Const.DEFAULT_RANKS;
+        }
+        
+        this.cards = cards as List<Card>;
+        this.jokers = jokers;
+        this.numJokers = numJokers;
+        this.rebuild = rebuild;
+        this.reshuffle = reshuffle;
+        this.ranks = ranks;
+        this.decksUsed = 0;
+
+        if (build)
+        {
+            Build();
+        } 
+    }
+
+
+    public static Deck operator+ (Deck deck, object other)
+    {
+        Deck newDeck;
+
+        if (other is Stack)
+        {
+            List<Card> cardList = new List<Card>();
+            
+            cardList.AddRange(deck.cards);
+            
+            Stack otherStack = (Stack)other;
+            cardList.AddRange(otherStack.cards);
+            
+            newDeck = new Deck(cards: (object)cardList);
+        }
+        else if (other is Deck)
+        {
+            List<Card> cardList = new List<Card>();
+            
+            cardList.AddRange(deck.cards);
+            
+            Deck otherDeck = (Deck)other;
+            cardList.AddRange(otherDeck.cards);
+            
+            newDeck = new Deck(cards: (object)cardList);
+        }
+        else if (other is List<Card>)
+        {
+            List<Card> cardList = new List<Card>();
+            
+            cardList.AddRange(deck.cards);
+            
+            List<Card> otherCards = other as List<Card>;
+            cardList.AddRange(otherCards);
+            
+            newDeck = new Deck(cards: (object)cardList);
+        }
+        else
+        {
+            throw new System.ArgumentException("Object on the right side of '+' must be of"
+                + " type Stack or Deck or be a list of Card instances");
+        }
+        
+        return newDeck;
+    }
+
+
+    public string Repr()
+    {
+        return $"Deck(cards={Cards})";
+    }
+
+
+    public void Build(bool jokers = false, int numJokers = 0)
+    {
+        if (jokers == false)
+        {
+            jokers = this.jokers;
+        }
+
+        if (numJokers == 0)
+        {
+            numJokers = this.numJokers;
+        }
+
+        this.decksUsed += 1;
+
+        Cards.AddRange(Tools.BuildCards(jokers, numJokers));
+    }
+
+
+    public Stack Deal(int num = 1, bool rebuild = false, bool shuffle = false, 
+        string end = Const.TOP)
+    {
+        if (num <= 0)
+        {
+            throw new System.ArgumentException("The 'num' parameter must be >= 1.");
+        }
+        
+        List<Card> dealtCards = new List<Card>();
+        int _num = num;
+        
+        if (!rebuild)
+        {
+            rebuild = this.rebuild;
+        }
+
+        if (!shuffle)
+        {
+            shuffle = this.reshuffle;
+        }
+
+        Card card;
+        int n;
+
+        while (num > 0)
+        {
+            n = _num - num;
+
+            try
+            {
+                if (end == Const.TOP)
+                {
+                    card = Cards[0];
+                    Cards.RemoveAt(0);
+
+                }
+                else
+                {
+                    card = Cards[-1];
+                    Cards.RemoveAt(-1);
+                }
+
+                dealtCards[n] = card;
+                num -= 1;
+            }
+            catch (System.IndexOutOfRangeException)
+            {
+                if (Size == 0)
+                {
+                    if (rebuild)
+                    {
+                        Build();
+                        
+                        if (shuffle)
+                        {
+                            Shuffle();
+                        }
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }     
+        }
+        
+        return new Stack(cards: dealtCards);
+    }
+
+
+    //===============================================================================
+    // Helper Functions
+    //===============================================================================
+    
+    public static Deck ConvertToDeck(Stack stack)
+    {
+        List<Card> cardStack = stack.cards;
+
+        return new Deck(cardStack);
+    }
+}
